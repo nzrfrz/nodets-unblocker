e969f89aeea627762c5a98e536b21765f33bb05b
nzrfrz <nzr.frz@gmail.com>
2024-12-14 12:24:59 +0700

    setup server and unblocker library

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+
+import path from "path";
+import express from "express";
+import bodyParser from "body-parser";
+import compression from "compression";
+
+var Unblocker = require('unblocker');
+
+const app = express();
+
+app.use(compression());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+const startServer = async () => {
+  try {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server Running on:\n http://localhost:${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Server Error: \n", error.toString());
+  }
+};
+
+const unblockerConfig = {
+  prefix: '/proxy/',
+};
+
+const unblocker = new Unblocker(unblockerConfig);
+
+app.use(unblocker);
+
+startServer();
